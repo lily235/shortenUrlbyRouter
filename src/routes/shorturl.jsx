e27b3003d2a28e7fed24fc6cb64507c535111493
lily235 <116597430+lily235@users.noreply.github.com>
2023-06-08 18:18:15 +0100

    add window.location.host

--- src/routes/shorturl.jsx
+++ src/routes/shorturl.jsx
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 export default function ShortUrl(){
     const location = useLocation();
     return(
-        //<div>hello from{window.location.host}{location.pathname}</div>
-        <div>Hello {location.pathname}</div>
+        <div>hello from{window.location.host}{location.pathname}</div>
+        //<div>Hello {location.pathname}</div>
     )
 }
