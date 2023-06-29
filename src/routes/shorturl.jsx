9a9f0627210ba65c048573270f982d2abd22f896
lily235 <116597430+lily235@users.noreply.github.com>
2023-06-29 10:39:07 +0100

    window.location.href

--- src/routes/shorturl.jsx
+++ src/routes/shorturl.jsx
@@ -2,6 +2,7 @@
 //   useLoaderData,
 //   useLocation,
 // } from 'react-router-dom';
+import * as React from 'react';
 
 import { useLoaderData } from 'react-router-dom';
 
@@ -14,9 +15,22 @@ export async function loader({ params }){
 export default function ShortUrl(){
     const {shorturl } = useLoaderData();
     //const location = useLocation();
+    if(shorturl === "google")
+    {
+        // const RedirectPage = () => {
+        //     React.useEffect(() => {
+        //       window.location.replace('https://www.google.com')
+        //     }, [])
+        //   }
+        window.location.href = 'https://www.google.com';
+          
+
+    }else{
     return(
        // <div>hello from {window.location.host}{location.pathname}!</div>
+       
        <div>{shorturl}</div>
     )
+    }
 }
 
